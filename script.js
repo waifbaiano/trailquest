04ac021394df7e0cd4119a999e86de5e1304377b
waifbaiano <waifbaiano@gmail.com>
2026-04-04 23:38:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,31 @@ function showScreen(id) {
     }
 }
 
-// Verifica se o Organizador preencheu os objetivos para o botão "acordar"
+// Limpeza de campos de Login
+function sairLogin() {
+    document.getElementById('login-email').value = "";
+    document.getElementById('login-pass').value = "";
+    showScreen('screen-start');
+}
+
+function loginMestre() {
+    // Ao entrar com sucesso, também limpamos para segurança
+    const email = document.getElementById('login-email').value;
+    if(email.length > 0) {
+        document.getElementById('login-email').value = "";
+        document.getElementById('login-pass').value = "";
+        showScreen('screen-main-menu');
+    } else {
+        alert("Preencha o e-mail");
+    }
+}
+
+// Validação Objetivos (O botão "acorda" apenas com o Fixo preenchido)
 function checkObjectives() {
-    const val = document.getElementById('fixed-count').value;
+    const fixed = document.getElementById('fixed-count').value;
     const btn = document.getElementById('btn-map');
     
-    if (val > 0) {
+    if (fixed > 0) {
         btn.disabled = false;
         btn.classList.remove('btn-inactive');
         btn.classList.add('active-green');
@@ -24,13 +43,28 @@ function checkObjectives() {
     }
 }
 
-// PIN de 6 dígitos
+// Validação Grupos
+function checkGroups() {
+    const qty = document.getElementById('group-qty').value;
+    const btn = document.getElementById('btn-save-groups');
+    
+    if (qty > 1) {
+        btn.disabled = false;
+        btn.classList.remove('btn-inactive');
+        btn.classList.add('active-green');
+    } else {
+        btn.disabled = true;
+        btn.classList.add('btn-inactive');
+        btn.classList.remove('active-green');
+    }
+}
+
 function checkPIN(val) {
     if (val.length === 6) {
         if (val === "123456") {
-            alert("Código de Trilha Aceito!");
+            alert("Sucesso!");
         } else {
-            alert("Código Inválido");
+            alert("Código Errado");
             document.getElementById('input-pin').value = "";
         }
     }
